63a08e15aa873f29483f9bf3ac27872b6a99f8ce
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-20 12:54:44 -0700

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+import React from 'react';
+import CategoryFilter from './CategoryFilter';
+import NewTaskForm from './NewTaskForm';
+import TaskList from './TaskList';
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import { CATEGORIES, TASKS } from '../data';
 
 function App() {
   return (
@@ -13,7 +11,7 @@ function App() {
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={TASKS} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react';
+import Task from './TaskList';
 
-function TaskList() {
+function TaskList({ tasks }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {/* {tasks.map((task) => console.log(task.category))} */}
+      {tasks.map((task) => (
+        <Task key={task.text} text={task.text} category={task.category} />
+      ))}
     </div>
   );
 }
